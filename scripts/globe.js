6e633e857357e551146f851931c7a541544419df
Amela Aganovic <aganovia@mail.gvsu.edu>
2022-04-13 22:17:14 -0400

    Basic lighting added

--- scripts/globe.js
+++ scripts/globe.js
@@ -1,4 +1,3 @@
-// set scene and camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   50,
@@ -7,7 +6,6 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-// define renderer
 const renderer = new THREE.WebGLRenderer({
   antialias: true,
 });
@@ -16,14 +14,29 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
-// create the globe
+// colors for lighting
+var colors = {
+  red: 0xFF0000,
+  orange: 0xFF8000,
+  yellow: 0xFFFF00,
+  white: 0xFFFFFF,
+  green: 0x00FF00,
+  cyan: 0x00FFFF,
+  blue: 0x0000FF,
+  pink: 0xFF00FF,
+  purple: 0x7F00FF,
+  water: 0x1E5772
+};
+
+// create a sphere
 var radius = 5;
 
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(radius, 50, 30),
-  new THREE.MeshBasicMaterial({
-    // color: 0xff0000
+  new THREE.MeshPhongMaterial({
+    // color: colors.water,
     map: new THREE.TextureLoader().load("./textures/8081_earthmap10k.jpg"),
+    shininess: 100
   })
   // new THREE.ShaderMaterial({
   //   vertexShader: ,
@@ -31,58 +44,54 @@ const sphere = new THREE.Mesh(
   // })
 );
 
+// add sphere and adjust camera & sphere positions
 scene.add(sphere);
+camera.position.set(0, 10, 20);
 sphere.position.x = 0;
-sphere.position.y = 0;
-sphere.position.z = -15;
+sphere.position.y = 10;
+sphere.position.z = 5;
 
-// create the moon
-const moongeometry = new THREE.SphereGeometry(0.1, 32, 32);
-const moonMaterial = new THREE.MeshPhongMaterial({
-  // roughness: 5,
-  // metalness: 0,
-  map: new THREE.TextureLoader().load("./textures/moonmap4k.jpg"),
-  bumpMap: new THREE.TextureLoader().load("./textures/moonbump4k.jpg"),
-  bumpScale: 0.02,
-});
-
-const moon = new THREE.Mesh(moongeometry, moonMaterial);
-moon.receiveShadow = true;
-moon.castShadow = true;
-moon.position.x = 0;
-moon.position.y = 0;
-moon.position.z = -15;
-// moon.layers.set(0);
-scene.add(moon);
-
-// moon pivots around the globe
-var moonPivot = new THREE.Object3D();
-sphere.add(moonPivot);
-moonPivot.add(moon);
-
-// spin & controls
+// spin controls
 var spinControl = new SpinControls(sphere, radius, camera, renderer.domElement);
 var cameraSpinControl = new CameraSpinControls(camera, renderer.domElement);
+
+// lighting and shadows
+renderer.shadowMap.enabled = true;
+sphere.receiveShadow = true;
+sphere.castShadow = true;
 
 // account for browser window resizing
 window.addEventListener("resize", onWindowResize, false);
 cameraSpinControl.onWindowResize();
 
-// animate
+// set lighting
+function setLight() {
+  ambientLight = new THREE.AmbientLight(colors.white); // soft white light
+  scene.add(ambientLight);
+
+  var lightIntensity = 0.4;
+  var lightColor = colors.purple;
+  var pointLight = new THREE.PointLight(lightColor, lightIntensity);
+  pointLight.position.set(0, 10, 70); // centered
+  // pointLight.position.set(0, 14, 10); // kinda top lit
+  pointLight.decay = 2;
+  scene.add(pointLight);
+
+  // TODO delete later
+  const sphereSize = 1;
+  const pointLightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
+  scene.add(pointLightHelper);
+}
+
 function animate() {
   requestAnimationFrame(animate);
 
   // sphere.rotation.y -= 0.005;
 
-  moonPivot.rotation.y -= 0.005;
-  moonPivot.rotation.x = 0.5;
   renderer.render(scene, camera);
   spinControl.update();
 }
 
-animate();
-
-// account for window resizing
 function onWindowResize() {
   windowHalfX = window.innerWidth / 2;
   windowHalfY = window.innerHeight / 2;
@@ -91,3 +100,6 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
+
+setLight();
+animate();
